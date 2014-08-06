609283b5f0f4760dd6508bd0bff82463d565c3ff
Ryan Regalado <ryan@design48.net>
2014-08-05 21:25:41 -0700

    merge fn

--- server/js/WhackGame.js
+++ server/js/WhackGame.js
@@ -11,9 +11,38 @@ var WhackGame = function() {
             , columns: 7
             , timer: 20
         }
+        , setup = {}
+
+        /**
+         * Merge object b into a
+         * 
+         * @name merge
+         * @param {object} a -  object with keys to merge into
+         * @param {object} b - object with keys to merge from
+         * @returns {object} a - object a modified 
+         * @example 
+         * merge({a: 1}, {a: 2}); // returns {a: 2} 
+         * merge({a: 1}, {b: 2}); // returns {a: 2, b:2} 
+         * merge({a: 1}, {b: 2}); // returns {a: 2, b:2} 
+         * @method 
+         * @memberof WhackGame
+         * @author Ryan Regalado 
+         */
+        , merge = function(a,b) {
+            if (a && b) {
+                for (var key in b) {
+                    if (b.hasOwnProperty(key)) {
+                        a[key] = b[key];
+                    }
+                }
+            } else {
+                return;
+            }
+            return a;
+        }
     ;
 
-    function init() {
+    function init(opts) {
         return 'initing the game';
     }
 
@@ -21,6 +50,7 @@ var WhackGame = function() {
         init: init
         , state: state
         , messages: messages
+        , defaults: this.defaults
     }
 };
 
